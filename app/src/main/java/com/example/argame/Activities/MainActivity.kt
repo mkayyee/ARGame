ecd27b8c19fbac63704d938e5e7845e718ed32fc
mkayyee <moc.liamg.reversed@gmail.com>
2020-02-12 16:12:41 +0200

    Layout files for menu's, Fragment classes for Menu's, MenuController

--- app/src/main/java/com/example/argame/Activities/MainActivity.kt
+++ app/src/main/java/com/example/argame/Activities/MainActivity.kt
@@ -2,14 +2,32 @@ package com.example.argame.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.argame.Fragments.MenuFragmentController
+import com.example.argame.Interfaces.FragmentCallbackHandler
 import com.example.argame.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentCallbackHandler {
+
+    private val menuFragController = MenuFragmentController()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initMenuContainer()
+    }
 
+    private fun initMenuContainer() {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.main_menu_container, menuFragController)
+            .addToBackStack(null)
+            .commit()
+    }
 
+    // This is a callback fired from every menu's buttons
+    override fun onButtonPressed(btn: Button) {
+        // The callback's are forwarded to MenuFragmentController,
+        // that handles all the logic for these events
+        menuFragController.onButtonPressed(btn)
     }
 }
