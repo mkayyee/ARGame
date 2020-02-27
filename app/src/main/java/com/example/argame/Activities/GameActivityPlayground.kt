bead55405474f2ea856cc7e088c5cd23a3ecad9b
mkayyee <moc.liamg.reversed@gmail.com>
2020-02-27 19:06:29 +0200

    Duplicate problem finally fixed... (?)

--- app/src/main/java/com/example/argame/Activities/GameActivityPlayground.kt
+++ app/src/main/java/com/example/argame/Activities/GameActivityPlayground.kt
@@ -391,8 +391,10 @@ class GameActivityPlayground : AppCompatActivity(), FragmentCallbackListener,
         for (anchor in npcAnchors) {
             removeAnchorNode(anchor.anchorNode)
         }
+        spawnedNPCs.clear()
         anchorList.clear()
         npcAnchors.clear()
+        npcsAlive.clear()
         ducksInScene = false
         playerInScene = false
     }
--- app/src/main/java/com/example/argame/Model/NPCDataForLevels.kt
+++ app/src/main/java/com/example/argame/Model/NPCDataForLevels.kt
@@ -25,11 +25,11 @@ object NPCDataForLevels {
         val npc1 = NPCSpawnData(NPCType.MELEE, 0,0)
         val npc2 = NPCSpawnData(NPCType.SUPPORT, 10000, 1)
         val npc3 = NPCSpawnData(NPCType.RANGED, 18000, 2)
-        val npc4 = NPCSpawnData(NPCType.RANGED, 250000, 3)
+        val npc4 = NPCSpawnData(NPCType.RANGED, 25000, 3)
         val npc5 = NPCSpawnData(NPCType.RANGED, 30000, 4)
         val npc6 = NPCSpawnData(NPCType.MELEE, 0, 5)
         val npc7 = NPCSpawnData(NPCType.MELEE, 0, 6)
-        val npc8 = NPCSpawnData(NPCType.SUPPORT, 250000, 7)
+        val npc8 = NPCSpawnData(NPCType.SUPPORT, 40000, 7)
 
         val npcs = arrayOf(
             npc1, npc2, npc3, npc4, npc5, npc6, npc7, npc8)
